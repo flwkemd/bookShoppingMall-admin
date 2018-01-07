e5f196da8d6f8d3382a26ab0817e52925b4894dc
flwkemd <flwkemd@naver.com>
2018-01-07 03:03:13 +0100

    implemented login page

---
+++ src/main/java/com/adminportal/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.adminportal.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.adminportal.domain.User;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+	User findByUsername(String username);
+
+}
---
+++ src/main/java/com/adminportal/service/impl/UserSecurityService.java
@@ -0,0 +1,28 @@
+package com.adminportal.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.adminportal.domain.User;
+import com.adminportal.repository.UserRepository;
+
+@Service
+public class UserSecurityService implements UserDetailsService{
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+		User user = userRepository.findByUsername(username);
+		
+		if(user == null) {
+			throw new UsernameNotFoundException("아이디가 존재하지 않습니다.");
+		}
+		
+		return user;
+	}
+}
