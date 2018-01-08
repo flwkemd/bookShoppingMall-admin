7a518caa67323cfd203eb09deee4ff66f1a5d7e6
flwkemd <flwkemd@naver.com>
2018-01-08 04:21:15 +0100

    implemented add new book

---
+++ src/main/java/com/adminportal/service/BookService.java
@@ -0,0 +1,8 @@
+package com.adminportal.service;
+
+import com.adminportal.domain.Book;
+
+public interface BookService {
+
+	Book save(Book book);
+}
---
+++ src/main/java/com/adminportal/service/impl/BookServiceImpl.java
@@ -0,0 +1,21 @@
+package com.adminportal.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adminportal.domain.Book;
+import com.adminportal.repository.BookRepository;
+import com.adminportal.service.BookService;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@Override
+	public Book save(Book book) {
+		return bookRepository.save(book);
+	}
+
+}
